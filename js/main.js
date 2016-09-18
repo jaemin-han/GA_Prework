8c2cf65e6be2b16b5859fd993c656aa3e54eb419
jaeminhan <jaeminhan@Jaemins-MacBook-Pro-2.local>
2016-09-18 17:47:48 -0400

    9.18

--- js/main.js
+++ js/main.js
@@ -4,3 +4,40 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// if (cardOne === CardTwo) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
+// if (cardOne === CardThree) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
+// if (cardOne === CardFour) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
+// if (cardTwo === CardThree) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
+// if (cardThree === CardFour) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
+// if (cardThree === CardFour) {
+// 	alert ('You have a match!')
+// } else {
+// 	alert ('Sorry, try again.')
+// }
+
